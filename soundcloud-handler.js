78e99b50b153f9912745eebfc607e7f4f9673ba2
Benjamin San Souci <benjamin.sansouci@gmail.com>
2015-05-18 19:24:07 -0400

    removing commented out code

--- soundcloud-handler.js
+++ soundcloud-handler.js
@@ -55,23 +55,6 @@ function _post(url, jar, form, callback) {
   request(op, callback);
 }
 
-// https://api.soundcloud.com/i1/tracks/90836050/streams?client_id=b45b1aa10f1ac2941910a7f0d10f8e28&app_version=9a98f21
-//
-// https://api.soundcloud.com/tracks/90836050/plays?policy=ALLOW&client_id=b45b1aa10f1ac2941910a7f0d10f8e28&app_version=9a98f21
-//
-//
-// https://cf-media.sndcdn.com/1hLqwlscpWDd.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vMWhMcXdsc2NwV0RkLjEyOC5tcDMiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE0MzE5NzgxMTd9fX1dfQ__&Signature=tBk2wOrxnGv7esm4jQxl7IMPzLbuGHPxDizrdOvcc2VzIyQ2jH1x6dkSJXgs~xqd6vUp642Nk4rJF2JLiQnuE-2ab9exFJIcacuRghB5AhLfNOHU0kQVfj28vGVMlCBCarX5KGMRYSikqxqA7q-RXntu4~WL7uGetJ6g60yrjjCT8KDPTdYJnuqeotBPi~cRScFuT5jseE0glsQb0a7oj8PX7zLjbhU~gXTB~1ysh8akThqfm6TRhT6nPjhkDf5M9aANTDBlnp02ptZj7QceFCGd8i6CV8GTfd-Wh3WjK5Rqn9f7mL4rZQCLfyEU-EJ~5SMIAinP1VSeCHDA3fbN2g__&Key-Pair-Id=APKAJAGZ7VMH2PFPW6UQ
-
-
-
-// var url = "https://soundcloud.com/djgodcrono/rock-and-roll-skrillex";
-// _get(url, jar, function(err, res, html) {
-//   var matches = html.match(/https:\/\/api.soundcloud.com\/tracks\/([0-9]+)\/download/);
-//   if(matches.length < 2) throw new Error("Couldn't find download link with track id in it");
-
-
-// });
-
 var jar = request.jar();
 module.exports = function(trackID, socket) {
   _get("https://api.soundcloud.com/i1/tracks/"+trackID+"/streams?", jar, {
@@ -81,15 +64,12 @@ module.exports = function(trackID, socket) {
     var data = JSON.parse(html);
 
     request(data.http_mp3_128_url)
-      .on("data", function(data) {
-        socket.emit("track-buffer-data", {track: trackID, buffer: data});
-      })
-      .on("end", function() {
-        socket.emit("track-buffer-data-end", {track: trackID});
-      });
-    // var piping = request(data.http_mp3_128_url).pipe(fs.createWriteStream(url.split("/").pop() + '.mp3'));
-    // piping.on("end", function() {
+    .on("data", function(data) {
+      socket.emit("track-buffer-data", {track: trackID, buffer: data});
+    })
+    .on("end", function() {
+      socket.emit("track-buffer-data-end", {track: trackID});
+    });
 
-    // });
   });
 };
